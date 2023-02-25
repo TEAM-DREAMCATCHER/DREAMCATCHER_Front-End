2dfaee38e25ff7782a9260e131e1f4c4ad313f30
seominjeong <smj12938@gmail.com>
2023-02-26 00:00:40 +0900

    feat: sortOption 파일 생성

---
+++ src/pages/sort/community.tsx
@@ -0,0 +1,10 @@
+import SortOption from '../../components/community/sortOption/index'
+
+export default function Community() {
+    return (
+        <>
+            <div>커뮤니티입니다.</div>
+            <SortOption />
+        </>
+    )
+}

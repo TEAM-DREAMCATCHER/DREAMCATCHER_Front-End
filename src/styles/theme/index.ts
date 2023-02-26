b38dd1c703219639ce073771dabf59bd219c867a
sumi-0011 <selina2000@naver.com>
2023-02-27 00:01:48 +0900

    feat: font 디자인 테마 세팅

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,13 @@
+import colors from '@/styles/theme/colors'
+import fonts from '@/styles/theme/fonts'
+import layouts from '@/styles/theme/layouts'
+
+// NOTE: 예시 사용 방법 (추후 삭제 예정)
+// color: ${({ theme }) => theme.colors.white};
+// ${({ theme }) => theme.fonts.text26}
+
+const theme = {
+    fonts,
+}
+
+export default theme

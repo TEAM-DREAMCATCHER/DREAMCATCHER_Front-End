cdadabfcc970d763cc13fad851d4f4c2b25b1c1a
sumi-0011 <selina2000@naver.com>
2023-02-27 01:02:51 +0900

    feat: 인증 request interceptor 세팅

---
+++ src/apis/index.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const request = axios.create({
+    baseURL: '',
+})
+
+request.interceptors.request.use(
+    function (config) {
+        const access_token = localStorage.getItem('access-token')
+
+        if (access_token === null) {
+            alert('로그인이 필요합니다. ')
+            throw new Error('access token이 없습니다.')
+        }
+
+        config.headers['Content-Type'] = 'application/json'
+        config.headers.Authorization = `Bearer ${access_token}`
+        return config
+    },
+    async function (error) {
+        return await Promise.reject(error)
+    }
+)
+
+export default request

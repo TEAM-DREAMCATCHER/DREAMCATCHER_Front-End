114514e6edfe9ec06c9818bac8cc56f18c460c6d
Jaehyeon <jaehyeonp74@gmail.com>
2023-02-26 19:49:06 +0900

    style: 파일명 수정 및 안 쓰는 코드 삭제

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import Home from '../pages/home'
 import ErrorPage from '../pages/ErrorPage'
-import Recording from '../pages/recording/Recording'
+import Recording from '../pages/recording'
 
 const router = createBrowserRouter([
     {

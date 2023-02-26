845bd29488e44eb7a4a73239edb26a9a072eae47
seona.bang <sit.or.seona@gmail.com>
2023-02-26 17:25:35 +0900

    feat: recording 푸터 체크박스 state 추가

--- src/components/record/footer/index.tsx
+++ src/components/record/footer/index.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react'
 import { Footer, Checkbox } from './style'
 
 export default function RecordFooter() {
+    const [isPublic, setIsPublic] = useState<boolean>(false)
+
     return (
         <Footer>
             <Checkbox>
-                <label htmlFor="visibility">커뮤니티에 공개</label>
-                <input type="checkbox" name="visibility" id="visibility" />
+                <label htmlFor="checkbox">커뮤니티에 공개</label>
+                <input
+                    type="checkbox"
+                    name="checkbox"
+                    id="checkbox"
+                    checked={isPublic}
+                    onChange={(e) => {
+                        setIsPublic(e.target.checked)
+                    }}
+                />
             </Checkbox>
         </Footer>
     )

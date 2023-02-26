ab4f8128a8b6b381502caa838b14bca9a6b31793
sumi-0011 <selina2000@naver.com>
2023-02-26 13:44:35 +0900

    FEAT: 절대경로 설정

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,3 +1,5 @@
+import ErrorPage from '@/pages/ErrorPage'
+import Home from '@/pages/home'
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import Home from '../pages/home'

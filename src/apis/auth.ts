e6d09f44bd8e95d78b4de9bf0073d2f77b9baccf
sumi-0011 <selina2000@naver.com>
2023-02-26 23:14:07 +0900

    feat: 아이디 중복 체크 API 수정

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,8 +1,12 @@
-import request from '.'
+import axios from 'axios'
+
+const ID_DUPLICATE_CHECK_BASE_URL = import.meta.env.VITE_PUBLIC_VALIDATE_DUPLICATE_URL
+const SIGN_UP_BASE_URL = import.meta.env.VITE_PUBLIC_SIGNUP_URL
 
 export const idDuplicateCheckAPI = async (id: string): Promise<boolean> => {
     try {
-        const response = await request.get('?username=' + id)
+        const response = await axios.get(ID_DUPLICATE_CHECK_BASE_URL + '?username=' + id)
+
         return response.data.result
     } catch (error) {
         throw error

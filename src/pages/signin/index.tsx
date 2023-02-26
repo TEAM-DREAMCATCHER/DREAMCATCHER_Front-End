d686a76bfc527ecbad0508b6b390797d07f95ebc
sumi-0011 <selina2000@naver.com>
2023-02-27 01:56:21 +0900

    feat: 로그인 api 연결

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,13 +1,24 @@
+import { loginAPI } from '@/apis/auth'
 import BackIcon from '@/components/common/icons/BackIcon'
 import InputItem from '@/components/signup/InputItem'
 import { Button, SigninLayout, Header, Heading, Main, SignupButton } from '@/pages/signin/styles'
 import useInput from '@/pages/signup/useInput'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Signin() {
+    const navigate = useNavigate()
+
     const [id, handleIdChange] = useInput()
     const [password, handlePasswordChange] = useInput()
 
+    const onAction = async () => {
+        try {
+            await loginAPI(id, password)
+            navigate('/welcome')
+        } catch (error) {
+            alert('로그인에 실패했습니다.')
+        }
+    }
     return (
         <SigninLayout>
             <Header>
@@ -32,7 +43,7 @@ function Signin() {
                     value={password}
                     placeholder="비밀번호 입력"
                 />
-                <Button>로그인</Button>
+                <Button onClick={onAction}>로그인</Button>
                 <SignupButton to="/signup">아이디가 없으신가요? 회원가입 하기</SignupButton>
             </Main>
         </SigninLayout>

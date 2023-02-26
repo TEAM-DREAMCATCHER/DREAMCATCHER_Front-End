c37c173cedb4d7320acdcc602116fdfda052f28b
soohyun <bmy1320@gmail.com>
2023-02-26 10:06:42 +0900

    feat: 임시저장

--- src/pages/community/community.tsx
+++ src/pages/community/community.tsx
@@ -1,7 +1,10 @@
+import Posting from '../../components/community/posting'
+
 export default function Community() {
     return (
         <>
             <div>커뮤니티입니다.</div>
+            <Posting />
         </>
     )
 }

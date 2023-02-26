316c985514572ce308aff29081bdd18088b6e87b
Jaehyeon <jaehyeonp74@gmail.com>
2023-02-26 11:20:03 +0900

    feat: Recording 라우팅 추가
    - Outlet 추가

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const home = () => {
-    return <div>hello world! home !</div>
+    return (
+        <div>
+            <Outlet />
+        </div>
+    )
 }
 
 export default home
---
+++ src/pages/recording/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const StyledRecord = styled.div`
+    background-color: black;
+    color: white;
+    display: flex;
+    width: 80vw;
+    height: 90vh;
+`

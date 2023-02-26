beaa5dd76f1ecfca525adb86bde1368f31572660
seona.bang <sit.or.seona@gmail.com>
2023-02-27 02:16:47 +0900

    refactor: recording 헤더 아이콘 svg 파일로 변경

--- src/components/record/header/style.ts
+++ src/components/record/header/style.ts
@@ -6,22 +6,19 @@ export const Header = styled.header`
     justify-content: space-between;
     align-items: center;
 `
-//export const CloseButton = styled.img.attrs({
-//    src: icnClose,
-//    alt: '창 닫기',
-//})`
-//    width: 32px;
-//    height: 32px;
-//    cursor: pointer;
-//    margin-left: -8px;
-//`
+export const CloseButton = styled.button.attrs({
+    alt: '창 닫기',
+})`
+    width: 32px;
+    height: 32px;
+    cursor: pointer;
+    margin-left: -14px;
+`
 
-//export const SaveButton = styled.img.attrs({
-//    src: icnSave,
-//    alt: '저장하기',
-//})`
-//    width: 28px;
-//    height: 28px;
-//    cursor: pointer;
-//    margin-right: -5px;
-//`
+export const SaveButton = styled.button.attrs({
+    alt: '저장하기',
+})`
+    width: 28px;
+    height: 29px;
+    cursor: pointer;
+`

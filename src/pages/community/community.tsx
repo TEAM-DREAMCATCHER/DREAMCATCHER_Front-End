f66bb626a3c1885a528bb55a71233e89b9116c04
soohyun <bmy1320@gmail.com>
2023-02-27 10:29:26 +0900

    feat: community 파일통일

--- src/pages/community/community.tsx
+++ src/pages/community/community.tsx
@@ -1,8 +1,7 @@
-import { Layout } from './styles'
+import { DetailButton, Layout } from './styles'
 import Category from '@/components/common/category'
 import SortOption from '@/components/common/sortOption'
 import Posting from '@/components/common/posting'
-import { DetailButton } from './style'
 
 export interface Info {
     id: string
--- src/pages/community/style.ts
+++
@@ -1,4 +0,0 @@
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-
-export const DetailButton = styled(Link)``

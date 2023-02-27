cd95fe69f39da8e0427cb66b8ee1cf62371af95c
seona.bang <sit.or.seona@gmail.com>
2023-02-27 17:55:58 +0900

    style: recording styles theme 적용

--- src/components/recordLayout/category/style.ts
+++ src/components/recordLayout/category/style.ts
@@ -1,11 +1,10 @@
 import styled from 'styled-components'
 
 export const CategoryLayout = styled.div`
-    background: #000;
+    background: ${({ theme }) => theme.colors.black};
 `
 export const CategoryList = styled.ul`
-    display: flex;
-    align-items: center;
+    ${({ theme }) => theme.layouts.flexStart};
     padding: 10px 22px;
     position: relative;
     height: 50px;
@@ -22,16 +21,15 @@ export const CategoryList = styled.ul`
     }
     &::-webkit-scrollbar-track {
         max-width: 10px;
-        background-color: black;
+        background-color: ${({ theme }) => theme.colors.black};
     }
 `
 export const CategoryItem = styled.li<{ isChosen: Boolean }>`
     padding: 5px 10px;
-    font-size: 13px;
-    font-weight: 400;
+    ${({ theme }) => theme.fonts.text13};
     cursor: pointer;
-    color: #fff;
-    text-shadow: #fff 1px 0 10px;
+    color: ${({ theme }) => theme.colors.white};
+    text-shadow: ${({ theme }) => theme.colors.white} 1px 0 10px;
     background: ${({ isChosen }) =>
         isChosen && 'linear-gradient(126.87deg, #635273 16.19%, #B9A1CF 83.65%)'};
     border-radius: ${({ isChosen }) => isChosen && '25px'};

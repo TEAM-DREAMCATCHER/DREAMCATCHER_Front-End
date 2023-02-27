013bed756216426d61fbd525c6ae7b172044066b
sumi-0011 <selina2000@naver.com>
2023-02-27 16:50:22 +0900

    fix: api interceptor 설정 변경

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,19 +1,11 @@
+import { checkAccessToken } from '@/apis/utlis'
 import axios from 'axios'
 
 const request = axios.create()
 
 request.interceptors.request.use(
     function (config) {
-        const access_token = localStorage.getItem('access-token')
-
-        if (access_token === null) {
-            alert('로그인이 필요합니다. ')
-            throw new Error('access token이 없습니다.')
-        }
-
-        config.headers['Content-Type'] = 'application/json'
-        config.headers.authorization = `Bearer ${access_token}`
-        return config
+        return checkAccessToken(config)
     },
     async function (error) {
         return await Promise.reject(error)

213b6f60bcef8fb0970402d4cc9e64125af25744
soohyun <bmy1320@gmail.com>
2023-02-27 14:52:00 +0900

    feat: Test페이지 빌드오류 수정

--- src/pages/Test/test.tsx
+++ src/pages/Test/test.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import AddButton from '@/components/common/AddButton'
-import Posting from '@/components/common/posting'
 
 const Test = () => {
     return (
@@ -13,8 +12,6 @@ const Test = () => {
             <AddButton shape="dot" />
             <AddButton shape="dot" size="sm" />
             <AddButton shape="dot" size="md" />
-
-            <Posting />
         </div>
     )
 }

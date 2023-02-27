83986a43a41d68fae4321182c47eade28e96a440
soohyun <bmy1320@gmail.com>
2023-02-27 16:48:21 +0900

    feat: 더보기 버튼 구현
    1. 게시물 4개씩 보여주기
    2. 마지막 게시물 밑에는 더보기 버튼 안보이기

--- src/pages/community/community.tsx
+++ src/pages/community/community.tsx
@@ -1,7 +1,8 @@
-import { DetailButton, Layout } from './styles'
+import { DetailButton, Layout, MoreBox, MoreButton } from './styles'
 import Category from '@/components/common/category'
 import SortOption from '@/components/common/sortOption'
 import Posting from '@/components/common/posting'
+import { useState } from 'react'
 
 export interface Info {
     id: string
@@ -16,6 +17,8 @@ export interface Info {
 }
 
 export default function Community() {
+    let [range, setRange] = useState(1)
+
     const mock = [
         {
             id: '4',
@@ -51,36 +54,123 @@ export default function Community() {
             pri: false,
             emoji: '1f601',
         },
+        {
+            id: '4',
+            userId: '조이',
+            content:
+                'dream dream dream dream dream dream dream dream dream dream dream dream dream dream',
+            createdAt: '2023-02-26',
+            updatedAt: '2023-02-26',
+            likeCount: 20,
+            category: 'joy',
+            pri: false,
+            emoji: '1f601',
+        },
+        {
+            id: '4',
+            userId: '조이',
+            content:
+                'dream dream dream dream dream dream dream dream dream dream dream dream dream dream',
+            createdAt: '2023-02-26',
+            updatedAt: '2023-02-26',
+            likeCount: 20,
+            category: 'joy',
+            pri: false,
+            emoji: '1f601',
+        },
+        {
+            id: '4',
+            userId: '조이',
+            content:
+                'dream dream dream dream dream dream dream dream dream dream dream dream dream dream',
+            createdAt: '2023-02-26',
+            updatedAt: '2023-02-26',
+            likeCount: 20,
+            category: 'joy',
+            pri: false,
+            emoji: '1f601',
+        },
+        {
+            id: '4',
+            userId: '조이',
+            content:
+                'dream dream dream dream dream dream dream dream dream dream dream dream dream dream',
+            createdAt: '2023-02-26',
+            updatedAt: '2023-02-26',
+            likeCount: 20,
+            category: 'joy',
+            pri: false,
+            emoji: '1f601',
+        },
+        {
+            id: '4',
+            userId: '조이',
+            content:
+                'dream dream dream dream dream dream dream dream dream dream dream dream dream dream',
+            createdAt: '2023-02-26',
+            updatedAt: '2023-02-26',
+            likeCount: 20,
+            category: 'joy',
+            pri: false,
+            emoji: '1f601',
+        },
+        {
+            id: '4',
+            userId: '조이',
+            content:
+                'dream dream dream dream dream dream dream dream dream dream dream dream dream dream',
+            createdAt: '2023-02-26',
+            updatedAt: '2023-02-26',
+            likeCount: 20,
+            category: 'joy',
+            pri: false,
+            emoji: '1f601',
+        },
     ]
+
+    const moreHandler = () => {
+        setRange(range + 1)
+    }
 
     return (
         <>
             <Layout>
                 <Category />
                 <SortOption />
-                {mock.map((info) => (
-                    <DetailButton
-                        to={`/Community/${info.id}`}
-                        state={{
-                            id: info.id,
-                            userId: info.userId,
-                            content: info.content,
-                            createdAt: info.createdAt,
-                            likeCount: info.likeCount,
-                            pri: info.pri,
-                            emoji: info.emoji,
-                        }}
-                    >
-                        <Posting
-                            key={info.userId}
-                            content={info.content}
-                            userId={info.userId}
-                            emoji={info.emoji}
-                            likeCount={info.likeCount}
-                            createdAt={info.createdAt}
-                        />
-                    </DetailButton>
-                ))}
+                {mock.map((info, idx) => {
+                    return idx < 4 * range ? (
+                        <DetailButton
+                            to={`/Community/${info.id}`}
+                            state={{
+                                id: info.id,
+                                userId: info.userId,
+                                content: info.content,
+                                createdAt: info.createdAt,
+                                likeCount: info.likeCount,
+                                pri: info.pri,
+                                emoji: info.emoji,
+                            }}
+                        >
+                            <Posting
+                                key={info.userId}
+                                content={info.content}
+                                userId={info.userId}
+                                emoji={info.emoji}
+                                likeCount={info.likeCount}
+                                createdAt={info.createdAt}
+                            />
+                        </DetailButton>
+                    ) : (
+                        ''
+                    )
+                })}
+                <MoreBox>
+                    {mock.length > 4 * range ? (
+                        <MoreButton onClick={() => moreHandler()}>더 보기</MoreButton>
+                    ) : (
+                        ''
+                    )}
+                </MoreBox>
             </Layout>
         </>
     )

e1b81e861c74543e0ca565c32c0b60cf08f8942d
sumi-0011 <selina2000@naver.com>
2023-02-27 20:30:08 +0900

    fix: navbar fix

--- src/components/common/navbar/index.tsx
+++ src/components/common/navbar/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import styled from 'styled-components'
 import HomeIcon from '../icons/HomeIcon'
 import navMoonIcon from '../../../../public/assets/icon/navMoon.png'
 import navMoonActiveIcon from '../../../../public/assets/icon/navMoon-active.png'
 
 import { NavLink, useLocation } from 'react-router-dom'
+import { Container } from '@/components/common/navbar/styles'
 
 const Navbar = () => {
     const { pathname } = useLocation()
@@ -14,7 +13,6 @@ const Navbar = () => {
             <nav>
                 <NavLink to="/home">{pathname === '/home' ? <HomeIcon /> : <HomeIcon />}</NavLink>
             </nav>
-
             <nav>
                 <NavLink to="/community">
                     {pathname === '/community' ? (
@@ -29,18 +27,3 @@ const Navbar = () => {
 }
 
 export default Navbar
-
-const Container = styled.div`
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 95px;
-    max-width: 430px;
-    width: 100%;
-    height: 80px;
-    background: linear-gradient(180deg, #0b0711 0%, rgba(11, 7, 17, 0.9) 100%);
-    border-radius: 30px 30px 0px 0px;
-`
---
+++ src/components/common/navbar/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 95px;
+    max-width: 430px;
+    width: 100%;
+    height: 80px;
+    background: linear-gradient(180deg, #0b0711 0%, rgba(11, 7, 17, 0.9) 100%);
+    border-radius: 30px 30px 0px 0px;
+    margin: auto;
+`

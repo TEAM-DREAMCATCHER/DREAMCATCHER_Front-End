87b1b40fde075cfc37d2c2be5fd40ebc845f834e
Luceta <57941164+Luceta@users.noreply.github.com>
2023-02-27 14:40:52 +0900

    add: tab navbar

---
+++ src/components/common/navbar/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components'
+import HomeIcon from '../icons/HomeIcon'
+import navMoonIcon from '../../../../public/assets/icon/navMoon.png'
+import navMoonActiveIcon from '../../../../public/assets/icon/navMoon-active.png'
+
+import { NavLink, useLocation } from 'react-router-dom'
+
+const Navbar = () => {
+    const { pathname } = useLocation()
+
+    return (
+        <Container>
+            <nav>
+                <NavLink to="/home">{pathname === '/home' ? <HomeIcon /> : <HomeIcon />}</NavLink>
+            </nav>
+
+            <nav>
+                <NavLink to="/community">
+                    {pathname === '/community' ? (
+                        <img src={navMoonActiveIcon} alt="" />
+                    ) : (
+                        <img src={navMoonIcon} alt="" />
+                    )}
+                </NavLink>
+            </nav>
+        </Container>
+    )
+}
+
+export default Navbar
+
+const Container = styled.div`
+    position: fixed;
+    bottom: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 95px;
+    max-width: 430px;
+    width: 100%;
+    height: 80px;
+    background: linear-gradient(180deg, #0b0711 0%, rgba(11, 7, 17, 0.9) 100%);
+    border-radius: 30px 30px 0px 0px;
+`
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,22 +3,26 @@ import MenuIcon from '@/components/common/icons/MenuIcon'
 import Calendar from '@/components/home/calendar'
 import { Header, HomeLayout } from '@/pages/home/styles'
 import yetDeveloped from '@/utils/yetDeveloped'
+import Navbar from '@/components/common/navbar'
 
 function Home() {
     return (
-        <HomeLayout>
-            <Header>
-                <span onClick={yetDeveloped}>
-                    <AlertIcon />
-                </span>
-                <span onClick={yetDeveloped}>
-                    <MenuIcon />
-                </span>
-            </Header>
-            <main>
-                <Calendar />
-            </main>
-        </HomeLayout>
+        <>
+            <HomeLayout>
+                <Header>
+                    <span onClick={yetDeveloped}>
+                        <AlertIcon />
+                    </span>
+                    <span onClick={yetDeveloped}>
+                        <MenuIcon />
+                    </span>
+                </Header>
+                <main>
+                    <Calendar />
+                </main>
+            </HomeLayout>
+            <Navbar />
+        </>
     )
 }
 
